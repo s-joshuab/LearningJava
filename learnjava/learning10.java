fcd2719de732eda6f4689929fe68771fc6061102
Joshua Sacquiaten <joshuabsacquiaten@gmail.com>
2025-05-20 14:01:29 +0800

    ASDSAD

--- learnjava/learning10.java
+++ learnjava/learning10.java
@@ -2,6 +2,17 @@ package learnjava;
 
 public class learning10 {
     public static void main(String[] args) {
-        
+        // 2D Array
+
+        String users[][] = {
+            {"cat", "cat123"},
+            {"dog", "dog123"},
+            {"lion", "lion123"},
+            {"chicken", "hen123"}
+        };
+
+        int grades[][] = new int[4][2];
+            {1, 2},
+            {}
     }
 }
